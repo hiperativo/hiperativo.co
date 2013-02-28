773e57bbe8182f17aabcd5eb98eb7836ccb32cd9
Pedro Maciel <pedrozath@gmail.com>
2013-02-28 11:36:28 -0300

    update

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Hiperativo::Application.routes.draw do
 	get "maintenance" => "errors#maintenance"
-	get "about" => "persons#index"
-	match ":action" => "pages"
-	root :to => "persons#index"
+	get "blog" => "posts#index"
+	root :to => "pages#index"
+
 end
